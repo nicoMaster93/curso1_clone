68ba8a1744ce8826a2168aa16ba7eca424a807e7
Nicolas Hernandez <juan.hernandez@proveedores.lineadirecta.com.co>
2023-02-08 17:15:13 -0500

    Se modifica para que sean componentes de clase

--- app-clone/src/App.js
+++ app-clone/src/App.js
@@ -1,35 +1,40 @@
-import {Testimonio} from './components/Testimonio';
+import React from "react";
+import Testimonio from './components/Testimonio';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div className='container-principal'>
-        <h1>Listado de personajes</h1>
-        <Testimonio 
-        nombre="Jake " 
-        empresa="en Avatar" 
-        cargo="Personaje de la Película" 
-        imagen="jake.jpeg"
-        texto="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae laudantium odit, assumenda ex, perferendis natus reiciendis molestiae corporis unde iure deserunt incidunt soluta animi facilis, eligendi rerum repellendus placeat rem!" 
-        />
-        <Testimonio 
-        nombre="Neytir " 
-        empresa="en Avatar" 
-        cargo="Personaje de la Película" 
-        imagen="Neytir.webp"
-        texto="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae laudantium odit, assumenda ex, perferendis natus reiciendis molestiae corporis unde iure deserunt incidunt soluta animi facilis, eligendi rerum repellendus placeat rem!" 
-        />
-        <Testimonio 
-        nombre="Ronal " 
-        empresa="en Avatar" 
-        cargo="Personaje de la Película" 
-        imagen="ronal.webp"
-        texto="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae laudantium odit, assumenda ex, perferendis natus reiciendis molestiae corporis unde iure deserunt incidunt soluta animi facilis, eligendi rerum repellendus placeat rem!" 
-        />
+/* Componentes de clase */
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <div className='container-principal'>
+          <h1>Listado de personajes</h1>
+          <Testimonio 
+          nombre="Jake " 
+          empresa="en Avatar" 
+          cargo="Personaje de la Película" 
+          imagen="jake.jpeg"
+          texto="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae laudantium odit, assumenda ex, perferendis natus reiciendis molestiae corporis unde iure deserunt incidunt soluta animi facilis, eligendi rerum repellendus placeat rem!" 
+          />
+          <Testimonio 
+          nombre="Neytir " 
+          empresa="en Avatar" 
+          cargo="Personaje de la Película" 
+          imagen="Neytir.webp"
+          texto="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae laudantium odit, assumenda ex, perferendis natus reiciendis molestiae corporis unde iure deserunt incidunt soluta animi facilis, eligendi rerum repellendus placeat rem!" 
+          />
+          <Testimonio 
+          nombre="Ronal " 
+          empresa="en Avatar" 
+          cargo="Personaje de la Película" 
+          imagen="ronal.webp"
+          texto="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae laudantium odit, assumenda ex, perferendis natus reiciendis molestiae corporis unde iure deserunt incidunt soluta animi facilis, eligendi rerum repellendus placeat rem!" 
+          />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- app-clone/src/components/Testimonio.jsx
+++ app-clone/src/components/Testimonio.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import '../estilos/Testimonio.css';
 /* Exportacion Nombrada */
-export function Testimonio(props){
-  return(
-    <div className="contenedor-testi">
-      <img src={require(`../imagenes/${props.imagen}`)} alt={`Foto de ${props.nombre}`} className="imagen-testi" />
-      <div className="contenedor-texto" >
-        <p className="nombre-testi" >{props.nombre} <b>{props.empresa}</b></p>
-        <p className="cargo-testi">{props.cargo}</p>
-        <p className="texto-testi">{props.texto}</p>
+class Testimonio extends React.Component {
+  render(){
+    return(
+      <div className="contenedor-testi">
+        <img src={require(`../imagenes/${this.props.imagen}`)} alt={`Foto de ${this.props.nombre}`} className="imagen-testi" />
+        <div className="contenedor-texto" >
+          <p className="nombre-testi" >{this.props.nombre} <b>{this.props.empresa}</b></p>
+          <p className="cargo-testi">{this.props.cargo}</p>
+          <p className="texto-testi">{this.props.texto}</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+export default Testimonio;
